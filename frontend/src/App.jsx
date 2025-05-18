77e48d64c6f65530c9d70c720699a7da5a2b0576
HimakaraL <himakaral@outlook.com>
2025-05-18 10:17:02 +0530

    Backend created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,16 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signin />} />
-        <Route path="/signin" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
